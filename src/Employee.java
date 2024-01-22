a071a5f65600dac983afc78df4454b5a6c067830
Devshree Moghe <devshree0602@gmail.com>
2024-01-22 16:07:40 +0530

    Attendance added

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,11 @@
 public class Employee {
+    String name;
+    Integer id;
+
+    boolean attendance;
+
+    Employee(String n,Integer i){
+        this.name=n;
+        this.id=i;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,21 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Employee e=new Employee("abc",123);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int check=(int)(Math.random()*10)%2;
+
+        if(check==0){
+            e.attendance=false;
+        }
+        else{
+            e.attendance=true;
+        }
+        if(e.attendance){
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println("Employee is Absent");
         }
     }
 }
