8b208acf4f12afcb2a91a6934f3bb6139893c38b
Devshree Moghe <devshree0602@gmail.com>
2024-01-22 16:32:49 +0530

    Daily Wage calculated

--- src/Employee.java
+++ src/Employee.java
@@ -2,6 +2,8 @@ public class Employee {
     String name;
     Integer id;
 
+    Integer workhour=0;
+    Integer wageperhour=20;
     boolean attendance;
 
     Employee(String n,Integer i){
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         Employee e=new Employee("abc",123);
 
+        //mod 2 is done to ensure result is either 0 or 1
         int check=(int)(Math.random()*10)%2;
 
         if(check==0){
@@ -14,11 +15,21 @@ public class Main {
         else{
             e.attendance=true;
         }
+
         if(e.attendance){
+            e.workhour=8;
             System.out.println("Employee is Present");
         }
         else{
             System.out.println("Employee is Absent");
         }
+
+        Main m_obj=new Main();
+        System.out.println("The salary of employee is :"+m_obj.calculateWage(e));
+
+
+    }
+    double calculateWage(Employee e1){
+        return e1.workhour*e1.wageperhour;
     }
 }
